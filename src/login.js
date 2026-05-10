fe00937a52097ff37f906f06392d934bbaae3887
Luan Jardim Moraes <146567289+luanj4rdim@users.noreply.github.com>
2026-05-10 16:34:54 -0400

    Refactor login function and update user data structure

--- src/login.js
+++ src/login.js
@@ -1,29 +1,60 @@
 const usuarios = [
     {
-        nome: 'Julio',
-        login: 'julio.lima',
-        senha: '123456'
+        id:  1,
+        nome: 'luan',
+        email: 'luan@gmail.com',
+        senha: '123456', 
+        expirado: false
     },
-    {
-        nome: 'Wedney',
-        login: 'wedney.silva',
-        senha: '654321'
+    {   id: 2,
+        nome: 'Tais',
+        email: 'Taissilva@hotmail.com',
+        senha: '654321',
+        expirado: false
     },
-    {
+    {   id: 3,
         nome: 'Bruna',
-        login: 'bruna.gomes',
-        senha: '123654'
+        email: 'bruna.gomes@gmail.com',
+        senha: '123654',
+        expirado: true
     }
 ];
 
-// Preciso de uma função capaz de receber duas entradas: Login e Senha. 
-// Ela deverá navegar em um lista pre-existente, com 3 usuários e então 
+export function realizarLogin(email, senha) {
+    // Laço
+    let usuarioEncontrado = null
+        usuarios.forEach(function(usuario){
+    
+        if(usuario.email === email)
+            usuarioEncontrado = usuario;
+        
+        });
+        if(usuarioEncontrado == null)
+            return 'Usuário não encontrado.';
+        if (usuarioEncontrado.email !== email) {
+            return 'Usuário não encontrado.';
+        }
+         if (usuarioEncontrado.senha !== senha ) {
+            return 'Senha incorreta para o usuario encontrado.';
+        }
+         if (usuarioEncontrado.expirado) {
+            return 'Credenciais Expiradas: Renove por favor.';
+        }
+
+    return 'Logado com sucesso.';
+}
+
+/*
+// Preciso de uma função capaz de receber duas entradas: Email e Senha e validar o login 
+// Ela deverá navegar em um lista pre-existente, com usuários dentro de um vetor contendo  id, nome, email, senha e expirado (boleano, pode ser true ou false);
 // retornar o texto “Logado com sucesso.” caso exista um usuário com login 
 // e senha iguais aos informados. Caso não exista um usuário com login e 
 // senha informados, uma mensagem dizendo “Usuário não encontrado.” 
 // deverá ser exibida. 
+// Caso a senha nao seja correta retornar: Senha incorreta para o usuário encontrado.
+// e caso o usuario tenha no campo expirado true retornar Credencial expirada;
 
-/*
+
     1. Entradas (O que é informado)
     - Login
     - Senha
@@ -36,16 +67,6 @@ const usuarios = [
     - Se nenhum usuário corresponder após percorrer toda a lista retornar "Usuário não encontrado."
 
     4. Saídas (O que o computador deve trazer como resposta)
-    - "Logado com sucesso." ou "Usuário não encontrado."
+    - "Logado com sucesso." ,"Usuário não encontrado.", " Credencial expirada", "Senha incorreta para o usuário encontrado.
+"
 */ 
-
-export function realizarLogin(login, senha) {
-    // Laço
-    for (let usuario of usuarios) {
-        if (usuario.login == login && usuario.senha == senha) {
-            return 'Logado com sucesso.';
-        }
-    }
-
-    return 'Usuário não encontrado.';
-}
